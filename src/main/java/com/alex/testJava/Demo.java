e0142bf5c38608be606750a2fc86a004d1a5a1f5
TENCENT\alexmnwang <moonboqi1@sina.com>
2019-06-05 20:40:06 +0800

    更新：内存分配demo

--- src/main/java/com/alex/testJava/Demo.java
+++ src/main/java/com/alex/testJava/Demo.java
@@ -54,4 +54,13 @@ public class Demo {
 	public void test3() throws Exception {
 		System.out.println(System.getProperty("java.vm.name"));
 	}
+	
+	private static final int _1MB = 1024 * 1024;
+	@Test
+	public void testGc() throws Exception {
+        byte[] allocation1 = new byte[2 * _1MB];
+        byte[] allocation2 = new byte[2 * _1MB];
+        byte[] allocation3 = new byte[2 * _1MB];
+        byte[] allocation4 = new byte[4 * _1MB];
+	}
 }
